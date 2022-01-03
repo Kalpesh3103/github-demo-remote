2398ad1bbe698b44fb8141b3e8fa91e10f9465db
Kalpesh3103 <77819268+Kalpesh3103@users.noreply.github.com>
2022-01-03 16:06:09 +0530

    inital commit of main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+#Add Implementation
+
+def add(x,y):
+    pass
+
+#Sub Implementation
+
+def sub(x,y):
+    pass
+
+#Multi Implimentation
+def mul(x,y):
+    pass
+
+#Div Implementatin
+def div(x,y):
+    pass
